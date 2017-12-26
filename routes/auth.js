9bbdea34df5f3b09f37fcf2aa1b376ed1cf92ecb
Rahul Nair <rahulnair25283@gmail.com>
2017-12-26 22:52:47 +1000

    using refresh token to get new access token

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const randomstring = require("randomstring");
+const tokenService = require("../service/tokenService");
+const db = require("../service/db");
 
 const router = express.Router();
 
@@ -9,16 +9,9 @@ router.post("/login", (req, res, next) => {
   const password = req.body.password;
 
   if (username === "rahul" && password === "nair") {
-    const accessToken = jwt.sign(
-      {
-        username
-      },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: 20
-      }
-    );
-    const refreshToken = randomstring.generate(48);
+    const accessToken = tokenService.createAccessToken(username);
+    const refreshToken = tokenService.createRefreshToken();
+    db.saveToken(refreshToken);
     res
       .contentType("application/json")
       .status(200)
@@ -30,18 +23,39 @@ router.post("/login", (req, res, next) => {
     const err = new Error("authentication failed");
     err.status = 400;
 
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
+    res
+      .contentType("application/json")
+      .status(400)
+      .json({
+        message: err.message,
+        status: err.status
+      });
   }
 });
 
-router.post("/logout", (req, res, next) => {
-  res.redirect("../");
+router.post("/token", (req, res, next) => {
+  const username = req.body.username;
+  const refreshTokenReceived = req.body.refreshToken;
+  if (db.tokenExists(refreshTokenReceived)) {
+    const accessToken = tokenService.createAccessToken(username);
+    res
+      .contentType("application/json")
+      .status(200)
+      .json({
+        accessToken
+      });
+  } else {
+    const err = new Error("refresh token invalid");
+    err.status = 400;
+
+    res
+      .contentType("application/json")
+      .status(400)
+      .json({
+        message: err.message,
+        status: err.status
+      });
+  }
 });
 
 module.exports = router;
